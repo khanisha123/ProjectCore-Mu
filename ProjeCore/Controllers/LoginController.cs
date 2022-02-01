0a6371f5b7a3d711ad56dd858c9a5c2819fb74a8
khanisha123 <khanisha@code.edu.az>
2022-02-02 00:14:47 +0400

    Log In ready

--- ProjeCore/Controllers/LoginController.cs
+++ ProjeCore/Controllers/LoginController.cs
@@ -1,15 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjeCore.DAL;
+using ProjeCore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ProjeCore.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        private readonly Context _context;
+
+        public LoginController(Context context)
         {
+            context = _context;
+        }
+        
+        
+        
+        public IActionResult GirisYap()
+        {
+            return View();
+        }
+        public async Task<IActionResult> GirisYap(Admin p)
+        {
+            var bilgiler = _context.Admins.FirstOrDefault(x => x.Kullanıcı == p.Kullanıcı && x.Şifre == p.Şifre);
+            if (bilgiler != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.Kullanıcı)
+                };
+                var useridentity = new ClaimsIdentity(claims, "Login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                RedirectToAction("Index", "Personel");
+            }
             return View();
         }
     }
--- ProjeCore/Startup.cs
+++ ProjeCore/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,7 +25,14 @@ namespace ProjeCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddControllersWithViews();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/GirisYap/";
+
+                });
 
             services.AddDbContext<Context>(opt =>
             {
@@ -39,12 +47,14 @@ namespace ProjeCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
